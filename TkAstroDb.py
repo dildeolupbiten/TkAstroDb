725dfc170426e9140581797116d73b9f9ad1a3bc
dildeolupbiten <tckutlu@gmail.com>
2019-01-07 21:37:08 +0300

    Update TkAstroDb.py

--- TkAstroDb.py
+++ TkAstroDb.py
@@ -14,6 +14,7 @@ import tkinter as tk
 import xml.etree.ElementTree
 import urllib.request as urllib
 import tkinter.messagebox as msgbox
+
 from datetime import datetime
 from tkinter.ttk import Progressbar, Treeview
 
