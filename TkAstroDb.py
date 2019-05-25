a453c9277b5ff7abab36f8c6917380ae196a247a
dildeolupbiten <tckutlu@gmail.com>
2019-05-25 23:53:45 +0300

    Update TkAstroDb.py

--- TkAstroDb.py
+++ TkAstroDb.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "1.4.8"
-__date__ = "25 May 2019"
 
 import os
 import sys
@@ -4024,7 +4023,7 @@ def about():
     name = "TkAstroDb"
     version, _version = "Version:", __version__
     build_date, _build_date = "Built Date:", "21 December 2018"
-    update_date, _update_date = "Update Date:", __date__
+    update_date, _update_date = "Update Date:", "25 May 2019"
     developed_by, _developed_by = "Developed By:", \
                                   "Tanberk Celalettin Kutlu"
     thanks_to, _thanks_to = "Special Thanks To:", \
