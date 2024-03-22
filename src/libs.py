e8fb52ad13baae5b0254160444b531aaf1e64b4a
dildeolupbiten <tckutlu@gmail.com>
2024-03-22 12:23:56 +0100

    Update libs.py

--- src/libs.py
+++ src/libs.py
@@ -6,21 +6,19 @@ import numpy as np
 import pandas as pd
 import swisseph as swe
 
-from queue import Queue
+from time import time
 from numpy import e, pi
 from os import path, getcwd
-from threading import Thread
 from webbrowser import open_new
 from scipy.integrate import quad
 from xml.etree import ElementTree
-from time import time, perf_counter
 from scipy.stats import binom, norm
 from datetime import datetime as dt
-from warnings import filterwarnings
 from matplotlib.figure import Figure
 from xlsxwriter.workbook import Workbook
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+)
 
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt, QObject, pyqtSignal, QThread
