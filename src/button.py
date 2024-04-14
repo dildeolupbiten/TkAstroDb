54f29c96115ef87a1af55a7c164075048df3efc9
dildeolupbiten <tckutlu@gmail.com>
2024-04-14 12:31:51 +0200

    Update

--- src/button.py
+++ src/button.py
@@ -8,16 +8,19 @@ class Worker(QObject):
     progress = pyqtSignal(int)
     finished = pyqtSignal(int)
 
-    def __init__(self, filters, filtered, widgets, parent=None):
+    def __init__(self, progress_bar, widgets, parent=None):
         super().__init__(parent)
-        self.filters = filters
-        self.filtered = filtered
+        self.progress_bar = progress_bar
         self.widgets = widgets
 
     def run(self):
+        filters = get_selections(self.widgets)
+        filtered = filter_database(self.widgets.database, filters)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setMaximum(len(filtered))
         get_chart_patterns(
-            filters=self.filters,
-            records=self.filtered,
+            filters=filters,
+            records=filtered,
             version=self.widgets.version,
             adb_version=self.widgets.adb_version,
             self=self
@@ -43,15 +46,12 @@ class Button(QFrame):
 
     def run(self):
         self.button.setEnabled(False)
-        filters = get_selections(self.widgets)
-        filtered = filter_database(self.widgets.database, filters)
         if self.worker_thread and self.worker_thread.isRunning():
             self.worker_thread.quit()
             self.worker_thread.wait()
         self.progress_bar.setVisible(True)
-        self.progress_bar.setMaximum(len(filtered))
         self.worker_thread = QThread()
-        self.worker = Worker(filters, filtered, self.widgets)
+        self.worker = Worker(self.progress_bar, self.widgets)
         self.worker.moveToThread(self.worker_thread)
         self.worker_thread.started.connect(self.worker.run)
         self.worker.progress.connect(self.progress)
@@ -63,6 +63,7 @@ class Button(QFrame):
         self.button.setEnabled(True)
         self.widgets.msgbox.info("Info", f"Analysis completed successfully.")
         self.progress_bar.setVisible(False)
+        self.progress_bar.setValue(0)
         self.worker_thread.quit()
         self.worker_thread.wait()
 
